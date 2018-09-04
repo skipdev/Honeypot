c43d9bb092fd4bca2b16e15299afc358ef3b74e0
Steph Barden <stephyx.web@gmail.com>
2018-09-04 15:46:49 +0100

    complete re-style, made the app look a lot nicer

--- js/donate.js
+++ js/donate.js
@@ -1,4 +1,9 @@
-function togglePopup() {
-   const popup = document.getElementById("myPopup");
+function toggleDonatePopup() {
+   const popup = document.getElementById("donatePopup");
+   popup.classList.toggle("show");
+}
+
+function toggleLeaderboardPopup() {
+   const popup = document.getElementById("leaderboardPopup");
    popup.classList.toggle("show");
 }
